166d5fa6ad20b12d611109586ad694ee3e0a1c5f
Miwha Geschwind <miwhag@gmail.com>
2020-01-02 03:37:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,13 +156,13 @@ def team_names
 end 
 
 
-# def team_colors(team_name)
-#   game_hash.each do |place, team| 
-#     if team[:team_name] == team_name 
-#       return team[:colors]
-#   end 
-# end 
-# end 
+def team_colors(team_name)
+  game_hash.each do |place, team| 
+    if team[:team_name] == team_name 
+      return team[:colors]
+  end 
+end 
+end 
 
 
 
